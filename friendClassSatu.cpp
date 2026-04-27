aa8b0ec85fda91ca0f996e7ea4f880114ed5a034
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-27 18:29:26 +0700

    mengeksekusi program

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -23,3 +23,24 @@ public:
     void displayAll(orang &a);
 };
 
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
