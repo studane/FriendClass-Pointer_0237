fc79111f1928b59d8511cf0a718b41d26387c910
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-27 18:12:22 +0700

    pointer reference refmhs

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -12,3 +12,7 @@ int main (){
     mahasiswa mhs{1}; // Object mhs
     mhs.showNim(); //Member Access Operator
 
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
+
