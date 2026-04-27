ddd22f65407927a07021ea31974aa929c034a58c
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-27 18:31:21 +0700

    menambah perintah dan menjalankan program

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -10,3 +10,15 @@ public:
     friend void setNama(mahasiswa &a, string);
 };
 
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
