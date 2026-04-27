d61e105cfffce04f483c5f3623888ba93324dbc7
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-27 18:12:46 +0700

    pointer dereference pmhs

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -16,3 +16,8 @@ int main (){
     refMhs.nim = 2; //Member Access Operator
     mhs.showNim();
 
+    mahasiswa *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
